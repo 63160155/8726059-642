0eef81651a63f1cc812818c87023ce26670e2616
63160155 <95855947+63160155@users.noreply.github.com>
2022-04-01 11:53:52 +0700

    final

--- lab05/validation.js
+++ lab05/validation.js
@@ -1,20 +1,21 @@
 function formValidation() {
-    var uid = document.registform.userid;
-    var passid = document.registform.passid;
-    var uname = document.registform.username;
-    var uadd = document.registform.address; 
-    var ucountry = document.registform.country;
-    var uzip = document.registform.zip;
-    var uemail = document.registform.email;
-    var usex = document.registform.sex;
-    if (validateUserID(uid, 5, 12)) {
-        if (validatePassword(passid, 7, 12)) {
-            if (allletter(uname)) {
-                if (alphanumeric(uadd)) {
-                    if (countryselect (ucountry)) {
-                        if (allnumeric(uzip)) {
-                            if (validateEmail(uemail)) {
-                                if (validateGender(usex)) {}
+    var uid = document.regisform.userid;
+    var passid = document.regisform.passid;
+    var uname = document.regisform.username;
+    var uadd = document.regisform.address;
+    var ucountry = document.regisform.country;
+    var uzip = document.regisform.zip;
+    var uemail = document.regisform.email;
+    var usex = document.regisform.sex;
+
+    if(validateUserID(uid,5,12)){
+        if(validatePassword(passid,7,12)){
+            if(allLetter(uname)){
+                if(alphanumric(uadd)){
+                    if(countryselect(ucountry)){
+                        if(allnumeric(uzip)){
+                            if(validateEmail(uemail)){
+                                if(validateGender(usex)){}
                             }
                         }
                     }
@@ -24,128 +25,137 @@ function formValidation() {
     }
     return false;
 }
-function validateUserID(uid, min, max) {
+
+function validateUserID(uid,min,max){
     var error = "";
-    var illegalChars = /\W/; // allow letters, numbers, and underscores
-    if (uid.value == "") {
+    var illegalChars = /\W/;
+
+    if(uid.value == ""){
         uid.style.background = 'Yellow';
-        error = "กรุณาป้อน User IDV\n";
+        error = "กรุณาป้อน User ID\n";
         alert(error);
         uid.focus();
         return false;
-    } else if ((uid.value.length < min) || (uid.value.length > max)) {
-        uid.style.background = 'Yellow';
-        error = "User ID ต้องมีความยาว " + min + "-" + max + " ตัวอักษร\n";
+    }else if((uid.value.length < min) || (uid.value.length > max)){
+        uid.style.background = "Yellow";
+        error = "User ID ต้องมีความยาว "+ min + "-" + max + " ตัวอักษร\n";
         alert(error);
         uid.focus();
         return false;
-    } else if (illegalChars.test(uid.value)) {
-        uid. style.background = 'Yellow';
+    }else if(illegalChars.test(uid.value)){
+        uid.style.background = "Yellow";
         error = "User ID มีตัวอักษรที่ไม่ได้รับอนุญาต\n";
         alert(error);
         uid.focus();
         return false;
-    } else {
-        uid.style.background = 'White';
+    }else{
+        uid.style.background = "white"
     }
     return true;
 }
-function validatePassword(passid, min, max) {
+
+function validatePassword(passid,min,max){
     var error = "";
-    var illegalChars = /[\W_1]/; // allow only letters and numbers
-    
-    if (passid.value == "") {
-        passid. style.background = 'Yellow';
+    var illegalChars = /[\W_]/; //allow letters, numbers, and undderscores
+
+    if(passid.value == ""){
+        passid.style.background = 'Yellow';
         error = "กรุณาป้อน Password\n";
         alert(error);
         passid.focus();
         return false;
-    } else if ((passid.value. length < 7) || (passid.value. length > 15)) {
-        error = "Password ต้องมีความยาว " + min + "-" + max + " ตัวอักษร\n";
+    }else if((passid.value.length < 7 ) || (passid.value.length > 15)){
+        error = "Password ต้องมีความยาว " + min + "-" + max +" ตัวอักษร\n";
         passid.style.background = 'Yellow';
         alert(error);
-        passid. focus();
+        passid.focus();
         return false;
-    } else if (illegalChars.test(passid.value)) {
+    }else if(illegalChars.test(passid.value)){
         error = "Password มีตัวอักษรที่ไม่ได้รับอนุญาต\n";
         passid.style.background = 'Yellow';
         alert(error);
         passid.focus();
         return false;
-    } else if ((passid.value.search(/[a-zA-Z]+/) == -1) || (passid.value.search(/[0-9]+/) == -1)) {
+    }else if((passid.value.search(/[a-zA-Z]+/) == -1) || (passid.value.search(/[0-9]+/) == -1)){
         error = "Password ต้องมีตัวเลขอย่างน้อย 1 ตัว\n";
         passid.style.background = 'Yellow';
         alert(error);
         passid.focus();
         return false;
-    } else {
-        passid.style.background = 'White';
+    }else{
+        passid.style.background = 'white';
     }
     return true;
 }
-function allLetter(uname) {
-    var letters = /^[A-Za-z]+$/;
-    if (uname.value.match(letters)) {
+
+function allLetter(uname){
+    var letters = /^[a-zA-Z]+$/;
+    if(uname.value.match(letters)){
         return true;
-    } else {
-        alert("Username ต้องเป็นตัวอักษรเท่านั้น");
+    }else{
+        alert('Username ต้องเป็นตัวอักษรเท่านั้น');
         uname.focus();
         return false;
     }
 }
-function alphanumeric(uaddr) {
-    var letters = /^(0-9a-zA-Z)+$/;
-    if (uaddr.value.match(letters)) {
+
+function alphanumeric(uaddr){
+    var letters = /^[0-9a-zA-Z]+$/;
+    if(uaddr.value.match(letters)){
         return true;
-    } else {
+    }else{
         alert('Address ต้องเป็นตัวอักษรหรือตัวเลขเท่านั้น');
         uaddr.focus();
         return false;
     }
 }
-function countryselect (ucountry) {
-    if (ucountry.value == "Default") {
+
+function countryselect(ucountry){
+    if(ucountry.value == "Default"){
         alert('กรุณาเลือก Country');
         ucountry.focus();
-        return false;
-    } else {
         return true;
+    }else{
+        return false;
     }
 }
-function allnumeric(uzip) {
-    var numbers = /^[0-9]+$/;
-    if (uzip.value.match(numbers)) {
+
+function allnumeric(uzip){
+    var letters = /^[0-9a-zA-Z]+$/;
+    if(uzip.value.match(numbers)){
         return true;
-} else {
+    }else{
         alert('ZIP Code ต้องเป็นตัวเลขเท่านั้น');
         uzip.focus();
         return false;
     }
 }
-function validateEmail(uemail) {
-    var filter = /^([a-zA-Z0-9_\.\-))+\@(([a-zA-Z0-9\-))+\.)+( [a-zA-Z0-9]{2,4})+$/;
+
+function validateEmail(uemail){
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     
-    if (!filter.test(uemail.value)) {
+    if(!filter.test(uemail.value)){
         alert('Email ไม่ถูกต้อง');
-        uemail. focus();
-        return false;
-    } else {
+        uemail.focus();
         return true;
+    }else{
+        return false;
     }
 }
-function validateGender(usex) {
-    var X = 0
-    for (i = 0; i < usex.length; ++1) {
-        //alert(usex[i).checked)
-        if (usex[i].checked)
-            X++
+
+function validateGender(usex){
+    var x = 0;
+    for(i = 0; i < usex.length ; i++){
+        //alert(usex[i].checked)
+        if(usex[i].checked)
+            x++
     }
-    if (x == 0) {
+    if(x == 0){
         alert('กรุณาเลือกเพศ');
         return false;
-    } else {
-        alert('Form Successfully Submitted');
-        //window. location.reload()
+    }else{
+        alert('Form Successfully Submitted')
+        //window.location.reload()
         //return true;
     }
 }
